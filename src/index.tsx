7bfbeb3b38057333e44b4d529c7c2d9d822d780a
Mochammad Firdaus <firdausious@gmail.com>
2022-03-30 09:09:20 +0700

    chore: update libs

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+
 import { App } from "./App";
 
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+const container = document.getElementById("app");
+
+if (!container) {
+  throw new Error("Root element not found");
+}
+
+const root = createRoot(container);
+
+root.render(<App />);
